cc0c6550b86468ed790870b9f14ea1f77766b44c
tarunb12 <tarunb12@users.noreply.github.com>
2020-09-23 17:13:27 +0000

    deploy: c02155c5413444feaaea4ebbea48f513b4e7cbde

---
+++ precache-manifest.33d36d82919cd1240a4898a0cf4bbf7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c4514632b00d6a5b83273429c06cdc",
+    "url": "/animal-classifier/index.html"
+  },
+  {
+    "revision": "8caf961fb945fb723d72",
+    "url": "/animal-classifier/static/css/main.caf6128a.chunk.css"
+  },
+  {
+    "revision": "62f500d614caa4182c58",
+    "url": "/animal-classifier/static/js/2.6c9d27d0.chunk.js"
+  },
+  {
+    "revision": "2024e83eb8a6179c7e632afd0f74a4a2",
+    "url": "/animal-classifier/static/js/2.6c9d27d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8caf961fb945fb723d72",
+    "url": "/animal-classifier/static/js/main.40280da3.chunk.js"
+  },
+  {
+    "revision": "fcd2d6bb20799da22df5",
+    "url": "/animal-classifier/static/js/runtime-main.0f99dc76.js"
+  }
+]);
