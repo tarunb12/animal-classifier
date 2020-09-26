971f825aed4c03e69b1a21199eeb273e9ab9b800
Tarun Belani <tbelani@ufl.edu>
2020-09-25 22:33:03 -0400

    cors

--- lambda/classifier/app.py
+++ lambda/classifier/app.py
@@ -2,7 +2,7 @@ import json
 import logging
 from typing import Any, Dict
 
-from context.lambda_context import LambdaContext
+from lambda_context import LambdaContext
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
@@ -13,6 +13,13 @@ def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> Dict[str, A
 
     return {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Credentials": True,
+            "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,x-requested-with",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
+            "Content-Type": "application/json",
+        },
         "body": json.dumps({
             "message": "hello world",
             "animal": {
