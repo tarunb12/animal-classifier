a22de1f19c9e80f8d347ec6cf98c7bb105163336
Tarun Belani <tbelani@ufl.edu>
2020-10-02 11:17:19 -0400

    pillow dependency pulled from amazonlinux into layer

--- lambda/dependencies.sh
+++ lambda/dependencies.sh
@@ -1,5 +1,6 @@
+mkdir -p dependencies/python/lib/python3.7/
 docker build -t tflite_amazonlinux .
 docker run -d --name=tflite_amazonlinux tflite_amazonlinux
 docker cp tflite_amazonlinux:/usr/local/lib64/python3.7/site-packages dependencies/python/lib/python3.7/
 docker stop tflite_amazonlinux
-zip -r lib.zip dependencies/python
+zip -r dependencies/lib.zip dependencies/python
