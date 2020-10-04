023012a2f274891dff93da88647ac4214d403b7c
Tarun Belani <tbelani@ufl.edu>
2020-10-04 13:29:36 -0400

    lambda layers fix

--- lambda/classifier/animal.py
+++ lambda/classifier/animal.py
@@ -1,20 +1,33 @@
+import boto3
 import json
 import logging
+import numpy as np
+import os
+from tempfile import TemporaryFile
 import tflite_runtime.interpreter as tflite
-from typing import Any, Dict
+from typing import Any, Dict, List, Tuple
 
 from lambda_context import LambdaContext
+from model import retrieve_model
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
+s3 = boto3.client('s3')
+
+
 def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> Dict[str, Any]:
-    log.info('\n\nevent: %s\n\ncontext: %s\n', event, context)
-    try:
-        tflite.Interpreter()
-    except ValueError as e:
-        log.error(e)
-        log.info('test')
+    log.info(f'event: {event}')
+
+    # image = event["body"]["data"]["image"]
+    model_bucket = os.environ['MODEL_BUCKET']
+
+    log.info(model_bucket)
+
+    model_f, labels_f = retrieve_model(model_bucket, 'animal')
+
+    prediction = run_prediction(model_f)
+    label_translation = get_labels_map(labels_f)
 
     return {
         "statusCode": 200,
@@ -28,9 +41,28 @@ def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> Dict[str, A
                 "name": "dog",
                 "confidence": .98,
             },
-            # "prediction": {
-            #     "name": "shih-tzu",
-            #     "confidence": .87
-            # },
         }),
     }
+
+
+def run_prediction(model_path: str) -> List[List[float]]:
+    interpreter = tflite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    input_shape = input_details[0]['shape']
+    input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    interpreter.invoke()
+
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    print(output_data)
+    return output_data
+
+
+def get_labels_map(labels_path: str) -> Dict[int, str]:
+    with open(labels_path) as f:
+        return json.load(f)
---
+++ lambda/classifier/process.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+SIZE = 48
+
+def process_image(img):
+    # image = Image.frombytes(img, SIZE)
+    # old_size = image.size
+
+    # ratio = float(SIZE) / old_size
+    # new_size = tuple([int(x * ratio) for x in old_size])
+    
+    # image = image.resize(new_size, Image.ANTIALIAS)
+    pass
