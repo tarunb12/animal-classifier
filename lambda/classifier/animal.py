86b8cfcbef55b3c841a37a72e8de2c4bdb78716c
Tarun Belani <tbelani@ufl.edu>
2020-10-04 16:54:19 -0400

    improved lambda policies

--- lambda/classifier/animal.py
+++ lambda/classifier/animal.py
@@ -22,12 +22,10 @@ def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> Dict[str, A
     # image = event["body"]["data"]["image"]
     model_bucket = os.environ['MODEL_BUCKET']
 
-    log.info(model_bucket)
-
     model_f, labels_f = retrieve_model(model_bucket, 'animal')
 
-    prediction = run_prediction(model_f)
-    label_translation = get_labels_map(labels_f)
+    prediction = run_prediction(model_f).tolist()
+    predicted_animal, confidence = translate_prediction(prediction, labels_f)
 
     return {
         "statusCode": 200,
@@ -36,16 +34,14 @@ def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> Dict[str, A
             "Content-Type": "application/json",
         },
         "body": json.dumps({
-            "message": "hello world",
             "prediction": {
-                "name": "dog",
-                "confidence": .98,
+                "name": predicted_animal,
+                "confidence": confidence,
             },
         }),
     }
 
-
-def run_prediction(model_path: str) -> List[List[float]]:
+def run_prediction(model_path: str) -> List[List[np.float32]]:
     interpreter = tflite.Interpreter(model_path=model_path)
     interpreter.allocate_tensors()
 
@@ -59,8 +55,19 @@ def run_prediction(model_path: str) -> List[List[float]]:
     interpreter.invoke()
 
     output_data = interpreter.get_tensor(output_details[0]['index'])
-    print(output_data)
+    log.info(output_data)
     return output_data
+
+
+def translate_prediction(output_data: List[List[float]], labels_path: str) -> Tuple[str, float]:
+    label_translation = get_labels_map(labels_path)
+    log.info('Softmax output: %s', output_data[0])
+
+    predicted_i = np.argmax(output_data[0])
+    predicted_animal = label_translation[str(predicted_i)]
+    confidence = output_data[0][predicted_i]
+
+    return predicted_animal, confidence
 
 
 def get_labels_map(labels_path: str) -> Dict[int, str]:
