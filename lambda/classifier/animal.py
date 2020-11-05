3b1754b97d13402f9fa72c795f84753ddb0cfd76
Tarun Belani <tbelani@ufl.edu>
2020-11-05 11:01:29 -0500

    lambda functionality, model update

--- lambda/classifier/animal.py
+++ lambda/classifier/animal.py
@@ -3,15 +3,13 @@ import json
 import logging
 import numpy as np
 import os
-<<<<<<< HEAD
-from tempfile import TemporaryFile
-=======
->>>>>>> 72db02b5642f419064726ce30b237737258856f1
 import tflite_runtime.interpreter as tflite
 from typing import Any, Dict, List, Tuple
 
+# pylint: disable=no-name-in-module
 from lambda_context import LambdaContext
 from model import retrieve_model
+from process import process_image
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
@@ -22,13 +20,17 @@ s3 = boto3.client('s3')
 def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> Dict[str, Any]:
     log.info(f'event: {event}')
 
-    # image = event["body"]["data"]["image"]
+    body = json.loads(event["body"])
+    base64_image = body["data"]["image"]
     model_bucket = os.environ['MODEL_BUCKET']
 
     model_f, labels_f = retrieve_model(model_bucket, 'animal')
 
-    prediction = run_prediction(model_f).tolist()
+    prediction = run_prediction(model_f, base64_image)
     predicted_animal, confidence = translate_prediction(prediction, labels_f)
+
+    log.info(f'Prediction: {predicted_animal}')
+    log.info(f'Confidence: {confidence}')
 
     return {
         "statusCode": 200,
@@ -44,7 +46,7 @@ def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> Dict[str, A
         }),
     }
 
-def run_prediction(model_path: str) -> List[List[np.float32]]:
+def run_prediction(model_path: str, base64_image: str) -> List[List[float]]:
     interpreter = tflite.Interpreter(model_path=model_path)
     interpreter.allocate_tensors()
 
@@ -52,23 +54,22 @@ def run_prediction(model_path: str) -> List[List[np.float32]]:
     output_details = interpreter.get_output_details()
 
     input_shape = input_details[0]['shape']
-    input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+    input_data = process_image(base64_image, input_shape)
     interpreter.set_tensor(input_details[0]['index'], input_data)
 
     interpreter.invoke()
 
-    output_data = interpreter.get_tensor(output_details[0]['index'])
-    log.info(output_data)
-    return output_data
+    output_data = interpreter.get_tensor(output_details[0]['index'])[0]
+    return output_data.tolist()
 
 
-def translate_prediction(output_data: List[List[float]], labels_path: str) -> Tuple[str, float]:
+def translate_prediction(output_data: List[float], labels_path: str) -> Tuple[str, float]:
     label_translation = get_labels_map(labels_path)
-    log.info('Softmax output: %s', output_data[0])
+    log.info('Softmax output: %s', output_data)
 
-    predicted_i = np.argmax(output_data[0])
+    predicted_i = np.argmax(output_data)
     predicted_animal = label_translation[str(predicted_i)]
-    confidence = output_data[0][predicted_i]
+    confidence = output_data[predicted_i]
 
     return predicted_animal, confidence
 
--- lambda/classifier/process.py
+++ lambda/classifier/process.py
@@ -1,15 +1,28 @@
 import base64
 from io import BytesIO
+import numpy as np
 from PIL import Image, ImageOps
 
-def process_image(b64: str, desired_size: list):
+def process_image(b64: str, raw_desired_size: list):
     base64_decode = BytesIO(base64.b64decode(b64))
-    img = Image.open(base64_decode)
-    # image = Image.frombytes(img, SIZE)
-    # old_size = image.size
+    image = Image.open(base64_decode)
+    old_size = image.size
+    desired_size = tuple(raw_desired_size[1:3])
 
-    # ratio = float(SIZE) / old_size
-    # new_size = tuple([int(x * ratio) for x in old_size])
-    
-    # image = image.resize(new_size, Image.ANTIALIAS)
-    pass
+    ratio = float(max(desired_size)) / max(old_size)
+    new_size = tuple([int(x * ratio) for x in old_size])
+    image = image.resize(new_size, Image.ANTIALIAS)
+
+    processed_image = Image.new('RGB', desired_size)
+    processed_image.paste(
+        image,
+        (
+            (desired_size[0] - new_size[0]) // 2,
+            (desired_size[1] - new_size[1]) // 2,
+        ),
+    )
+
+    input_image = np.asarray(processed_image, dtype=np.float32)
+    input_image = input_image / 255.
+    input_image = np.reshape(input_image, raw_desired_size)
+    return input_image
